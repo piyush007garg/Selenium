418275492e00844c8bbed282c5e0f9184c504303
piyush.garg <piyush.garg@KELLGGNCPU0122.foxtons.co.uk>
2017-09-27 16:32:12 +0530

    testing 1

---
+++ src/static_and_global_variable.java
@@ -0,0 +1,26 @@
+
+public class static_and_global_variable {
+	
+	String name;
+	static int age;
+	
+
+	public static void main(String[] args) {
+
+sum();
+// sendmail();
+age =100;
+// name ="piyush";
+
+	}
+
+	
+	public static void sum(){ // Static function
+		
+	}
+	
+	public void sendmail(){ // non static function
+		
+		
+	}
+}
