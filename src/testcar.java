c2c6acbb658b93cfdf138e1a2ee74645f52c4ef4
piyush.garg <piyush.garg@KELLGGNCPU0122.foxtons.co.uk>
2017-09-27 16:09:19 +0530

    testing 1

---
+++ src/testcar.java
@@ -0,0 +1,38 @@
+
+public class testcar implements Interface{
+
+	@Override
+	public void foxtons() {
+		// TODO Auto-generated method stub
+		System.out.println();
+	}
+
+	@Override
+	public void page1() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void guru() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void kellton() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void clubmahendra() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+	}
+
+
